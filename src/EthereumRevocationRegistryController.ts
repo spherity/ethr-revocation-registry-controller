c4a07c58e3f111d1b059eb71a3ae79134fdbeeb1
Dennis von der Bey <dennis@vonderbey.eu>
2022-09-01 16:53:38 +0200

    feat: contract integration!

---
+++ src/EthereumRevocationRegistryController.ts
@@ -0,0 +1,43 @@
+/**
+ * A class that can be used to interact with the EIP-5539 contract on behalf of a local controller key-pair
+ */
+import {RevocationRegistry, factories} from "ethr-revocation-list/types/ethers-contracts";
+import {JsonRpcProvider, Provider} from "@ethersproject/providers";
+import {Signer} from "@ethersproject/abstract-signer";
+
+export const DEFAULT_REGISTRY_ADDRESS = '0x00000000000000000000000'
+
+export class EthereumRevocationRegistryController {
+  private registry: RevocationRegistry
+  private signer?: Signer
+  private address: string
+
+  constructor(
+    contract?: RevocationRegistry,
+    signer?: Signer,
+    chainNameOrId = 'mainnet',
+    provider?: Provider,
+    rpcUrl?: string,
+    address: string = DEFAULT_REGISTRY_ADDRESS
+  ) {
+    if (contract) {
+      this.registry = contract
+    } else if (provider || signer?.provider || rpcUrl) {
+      let prov = provider || signer?.provider
+      if(!prov) {
+        prov = new JsonRpcProvider(rpcUrl, chainNameOrId || 'any')
+      }
+      this.registry = new factories.RevocationRegistry__factory()
+        .attach(address || DEFAULT_REGISTRY_ADDRESS)
+        .connect(prov)
+    } else {
+      throw new Error('Either a contract instance or a provider or rpcUrl is required to initialize!')
+    }
+    this.signer = signer
+    this.address = address
+  }
+
+  async isRevoked(namespace: string, list: string, revocationKey: string): Promise<boolean> {
+    return this.registry.isRevoked(namespace, list, revocationKey);
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {
+  EthereumRevocationRegistryController
+} from "./EthereumRevocationRegistryController"
