f582389d2c1fbac6a9a9d4ce5cb593917e63eb56
Dennis von der Bey <dennis@vonderbey.eu>
2022-09-07 14:53:45 +0200

    fix: ganache tests

--- test/revocation.test.ts
+++ test/revocation.test.ts
@@ -1,6 +1,6 @@
 import { createProvider, sleep, startMining, stopMining } from './testUtils'
-import {RevocationRegistry, factories} from "ethr-revocation-list/types/ethers-contracts";
-import {EthereumRevocationRegistryController} from "../src";
+import {RevocationRegistry, factories} from "@spherity/ethr-revocation-registry/types/ethers-contracts";
+import {EthereumRevocationRegistryController, RevocationKeyPath} from "../src";
 
 jest.setTimeout(30000)
 
@@ -8,8 +8,16 @@ describe('EthrRevocationRegistryController', () => {
   let registry: RevocationRegistry
   let controller: EthereumRevocationRegistryController
   let accounts: string[]
-  const list = "0x3458b9bfc7963978b7d40ef225177c45193c2889902357db3b043a4e319a9628"
-  const revocationKey = "0x89343794d2fb7dd5d0fba9593a4bb13beaff93a61577029176d0117b0c53b8e6"
+  const exampleList = "0x3458b9bfc7963978b7d40ef225177c45193c2889902357db3b043a4e319a9628"
+  const exampleRevocationKey = "0x89343794d2fb7dd5d0fba9593a4bb13beaff93a61577029176d0117b0c53b8e6"
+
+  function generateRevocationKeyPathForAccount(account: string): RevocationKeyPath {
+    return {
+      namespace: account,
+      list: exampleList,
+      revocationKey: exampleRevocationKey,
+    }
+  }
 
   const web3Provider = createProvider()
 
@@ -21,28 +29,28 @@ describe('EthrRevocationRegistryController', () => {
     await registry.deployTransaction.wait()
     accounts = await web3Provider.listAccounts()
 
-    controller = new EthereumRevocationRegistryController(registry, web3Provider)
+    controller = new EthereumRevocationRegistryController({contract: registry, provider: web3Provider})
   })
 
   it('checks revocation status for default namespaces', async () => {
-    await expect(controller.isRevoked(accounts[0], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[1], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[2], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[3], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[4], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[5], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[6], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[7], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[8], list, revocationKey)).resolves.toEqual(false)
-    await expect(controller.isRevoked(accounts[9], list, revocationKey)).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[0]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[1]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[2]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[3]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[4]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[5]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[6]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[7]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[8]))).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[9]))).resolves.toEqual(false)
   })
 
   it('revokes key and then unrevokes it', async () => {
-    await expect(controller.isRevoked(accounts[0], list, revocationKey)).resolves.toEqual(false)
-    await controller.changeStatus(true, accounts[0], list, revocationKey)
-    await expect(controller.isRevoked(accounts[0], list, revocationKey)).resolves.toEqual(true)
-    await controller.changeStatus(false, accounts[0], list, revocationKey)
-    await expect(controller.isRevoked(accounts[0], list, revocationKey)).resolves.toEqual(false)
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[0]))).resolves.toEqual(false)
+    await controller.changeStatus(true, generateRevocationKeyPathForAccount(accounts[0]))
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[0]))).resolves.toEqual(true)
+    await controller.changeStatus(false, generateRevocationKeyPathForAccount(accounts[0]))
+    await expect(controller.isRevoked(generateRevocationKeyPathForAccount(accounts[0]))).resolves.toEqual(false)
   })
 
 })
